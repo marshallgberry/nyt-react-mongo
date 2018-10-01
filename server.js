b3663d2ea741167fef81de042e4e0e1160ead4f3
Marshall Berry <marshallgberry@gmail.com>
2018-10-01 11:19:40 -0400

    assets added

--- server.js
+++ server.js
@@ -3,20 +3,32 @@ const path = require("path");
 const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+mongoose.Promise = require("bluebird");
+
+mongoose.connect(
+    process.env.MONGODB_URI || "mongodb://localhost:27017/nytreact", 
+    (err) => err ? 
+        console.log(`Sorry, could not connect to the database.`) : 
+        console.log(`Connected to database!`));
+
 
 // Define middleware here
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cors());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
 // Define API routes here
-
+require("./routes/api")(app);
 // Send every other request to the React app
 // Define any API routes before this runs
-app.get("*", (req, res) => {
+app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
