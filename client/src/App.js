3d007359c475f5cf44df052d40cd54ebe4ef08b2
Marshall Berry <marshallgberry@gmail.com>
2018-10-01 11:40:42 -0400

    app.js/client updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,26 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Home from "./pages/Home";
+import Saved from "./pages/Saved";
+import Nav from "./components/Nav";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import './assets/styles/styles.css';
 
 class App extends Component {
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Router>
+          <div>
+            <Nav />
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/saved" component={Saved} />
+            </Switch>
+          </div>
+        </Router>
       </div>
-    );
+    )
   }
 }
 
